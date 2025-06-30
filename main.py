b92fbdaa45e4360994b8a9e525a882706bd4d560
Beta-Verse-Hub <baibhavkumarjha1@gmail.com>
2025-06-30 18:34:14 +0530

    Knowing if a window is closed and getting all window titles

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ import threading
 import keyboard
 from pynput.mouse import Controller
 from time import sleep
+import pygetwindow as gw
+
+
+AllStyles = []
+
+
+def addStyleText(text):
+    AllStyles.append("+ "+text)
+    if len(AllStyles) > 10:
+        AllStyles.pop(0)
 
 
 def input(window):
@@ -19,24 +29,48 @@ def input(window):
         if keyboard.is_pressed("esc"):
             window.destroy()
 
+
 def check_mouse(window):
+    
+    """
+    Monitors the mouse position and checks if the user has moved it over a certain distance.
+    If the user has moved the mouse over the specified distance, then it prints "+ John Cena's mouse" to the console.
+    
+    Args:
+        window: A Tkinter window instance to monitor and potentially destroy.
+    """
+
     mouse = Controller()
     breaks_for_check = {
         "distance" : [1,1]
     }
     old_mouse_position = [-1,-1]
+    open_windows = gw.getAllTitles()
 
-    while True:
+    while window.winfo_exists():
+    
         if breaks_for_check["distance"][0] == breaks_for_check["distance"][1]:
+
             if mouse.position != old_mouse_position:
+    
                 if old_mouse_position != [-1,-1] and (abs(mouse.position[0]-old_mouse_position[0]) > 150 or abs(mouse.position[1]-old_mouse_position[1]) > 150):
-                    print("+ John Cena's mouse")
+                    addStyleText("John Cena's mouse")
+
                 old_mouse_position = mouse.position
+
+        if len(gw.getAllTitles()) != len(open_windows):        
+            addStyleText("RECONSTRUCT WHAT!?!")
+            open_windows = gw.getAllTitles()
+
         for i in breaks_for_check:
             if breaks_for_check[i][0] == 0:
                 breaks_for_check[i][0] = breaks_for_check[i][1]
                 continue
             breaks_for_check[i][0] -= 1
+        
+        newest_window = gw.getAllTitles()
+        print(newest_window)
+    
         sleep(0.01)
 
 
@@ -73,7 +107,7 @@ window.wm_attributes('-transparentcolor', transparent_color)
 
 
 # Create a Label with the same background color as the transparent color
-Label(window, text="Hello", font=('Helvetica 18'), bg="#ffffff").pack(ipadx=100, ipady=150)
+Label(window, text=AllStyles, font=('Helvetica 18'), bg="#ffffff").pack(ipadx=100, ipady=150)
 
 
 # Start the main loop
